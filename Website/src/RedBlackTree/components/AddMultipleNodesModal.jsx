7ab0c4a5f8188de76d2d8db2b846e253af541e63
Nick Prinz <nickprinz8@gmail.com>
2024-06-15 17:55:49 -0500

    website- adding framer motion

--- Website/src/RedBlackTree/components/AddMultipleNodesModal.jsx
+++ Website/src/RedBlackTree/components/AddMultipleNodesModal.jsx
@@ -4,7 +4,7 @@ import RedBlackModal from "./RedBlackModal";
 export default function AddMultipleNodesModal({open, value, max, children, onClose }) {
   
     return <RedBlackModal onClose={onClose} open={open}>
-            <h1 className="text-xl text-zinc-200">Please wait while we add these values...</h1>
-            <progress className=" m-2 ml-0 mt-6 w-full h-6 rounded-full border-2 border-zinc-950 [&::-webkit-progress-bar]:bg-zinc-800 [&::-webkit-progress-bar]:rounded-full [&::-webkit-progress-value]:rounded-full [&::-moz-progress-bar]:rounded-full [&::-webkit-progress-value]:bg-red-500 [&::-moz-progress-bar]:bg-red-500" max={max} value={value}></progress>
+            <h1 className="text-xl text-zinc-200">Please wait while inserting these values...</h1>
+            <progress className=" m-2 ml-0 mt-6 mb-0 w-full h-3 rounded-full border-2 border-zinc-950 [&::-webkit-progress-bar]:bg-zinc-800 [&::-webkit-progress-bar]:rounded-full [&::-webkit-progress-value]:rounded-full [&::-moz-progress-bar]:rounded-full [&::-webkit-progress-value]:bg-red-500 [&::-moz-progress-bar]:bg-red-500" max={max} value={value}></progress>
         </RedBlackModal>;
   }

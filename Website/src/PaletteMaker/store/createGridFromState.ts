e6bc0abbc7f9bbe1991cdb9a9673a2a50f06c20a
Nick Prinz <nickprinz8@gmail.com>
2024-09-21 16:08:01 -0500

    pal- adding ability to create cell color directly

--- Website/src/PaletteMaker/store/createGridFromState.ts
+++ Website/src/PaletteMaker/store/createGridFromState.ts
@@ -9,26 +9,53 @@ export const createGridFromState = (state: PaletteState): ColorCell[][] => {
     const seed: Hsv = RGBtoHSV(state.seed);
 
     for (let i = 0; i < state.rowCount; i++) {
-        const hue = (seed.h + rowHueChange*i)%360;
+        const hue = wrap(seed.h + rowHueChange*i, 360);
         const rowCenter: Hsv = {h: hue, s:seed.s, v:seed.v};
         grid.push(makeNewRowFromCenter(rowCenter, i, state.shadeCount, state.lowSat, state.lowValue, state.highSat, state.highValue, state.hueShift, state.cellAdjustments[i]));//need to split row across seed
     }
 
     return grid;
-
 }
 
 export const createCellFromState = (state: PaletteState, row: number, column: number): ColorCell => {
-    const rowHueChange = 360/state.rowCount;
-    const grid: ColorCell[][] = [];
-    const seed: Hsv = RGBtoHSV(state.seed);
-    const centerHue = (seed.h + rowHueChange*row)%360;
+    const centerColor = getRowCenterColor(RGBtoHSV(state.seed), row, state.rowCount);
+    const interpolatedColor: Hsv = getInterpolatedColor(centerColor, column, state);
+    const resultColor = applyAdjustments(interpolatedColor, row, column, state);
+    return makeCellFromColor(resultColor, row, column);
+}
+
+const getRowCenterColor = (seed: Hsv, row:number, rowCount: number):Hsv => {
+    const rowHueChange = 360/rowCount;
+    const centerHue =  wrap(seed.h + (rowHueChange*row), 360);
+    return {h:centerHue, s: seed.s, v:seed.v};
+}
+
+const getEndColor = (centerColor: Hsv, endSat:number, endValue: number, hueShift: number, columnDiff:number):Hsv => {
+    const newHue = centerColor.h + hueShift*(columnDiff-1);//don't wrap this, helps with lerp
+    return {h:newHue, s: endSat, v:endValue};
+}
+
+const applyAdjustments = (startColor:Hsv, row: number, column: number, state: PaletteState): Hsv => {
+    //need to apply column and row adjustments later
+    return addHsv(startColor,state.cellAdjustments[row][column]);
+}
+
+const getInterpolatedColor = (centerColor: Hsv, column: number, state: PaletteState): Hsv => {
     const middleIndex = Math.ceil(state.shadeCount/2);
-    
+    let resultColor: Hsv;
+    if(column < middleIndex){
+        const lowColor = getEndColor(centerColor, state.lowSat, state.lowValue, state.hueShift, -middleIndex);
+        resultColor = lerpColor(lowColor, centerColor, column/middleIndex);
+    }
+    else if(column > middleIndex){
+        const highColor = getEndColor(centerColor, state.highSat, state.highValue, state.hueShift, state.shadeCount-1-middleIndex);
+        resultColor = lerpColor(highColor, centerColor, column/middleIndex);
 
-
-    return grid;
-
+    } else{
+        resultColor = centerColor;
+    }
+    resultColor.h = wrap(resultColor.h,360);
+    return resultColor;
 }
 
 const makeNewRowFromCenter = (centerColor: Hsv, rowNum: number, columns: number, lowSat: number, lowValue: number, highSat: number, highValue: number, hueShift: number, cellAdjustments: Hsv[]): ColorCell[] => {
@@ -45,19 +72,23 @@ const makeNewRowFromCenter = (centerColor: Hsv, rowNum: number, columns: number,
     hsvs = hsvs.map((x,i) => {
         return addHsv(x,cellAdjustments[i])
     });
-    const row = hsvs.map((x, i) => {return {
-        hexColor: HSVtoRGBHex(x),
-        lum: LumFromHsv(x),
-        id: `id#${rowNum}#${i}`,
-        rowNum: rowNum,
-        colNum: i,
-        hue: x.h,
-        sat: x.s,
-        val: x.v,
-        chr: ChromaFromHsv(x),
-    }});
+    const row = hsvs.map((x, i) => {return makeCellFromColor(x, rowNum, i)});
 
     return row;
+}
+
+const makeCellFromColor = (color: Hsv, row:number, column:number): ColorCell => {
+    return {
+        hexColor: HSVtoRGBHex(color),
+        lum: LumFromHsv(color),
+        id: `id#${row}#${column}`,
+        rowNum: row,
+        colNum: column,
+        hue: color.h,
+        sat: color.s,
+        val: color.v,
+        chr: ChromaFromHsv(color),
+    }
 }
 
 const fillColors = (lowColor: Hsv, highColor: Hsv, colorCount: number): Hsv[] => {

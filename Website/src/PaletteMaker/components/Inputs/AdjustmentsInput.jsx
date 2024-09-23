54371e8152a14b4534532765c4bfc5bb0faaf2c4
Nick Prinz <nickprinz8@gmail.com>
2024-09-23 00:50:58 -0500

    pal- adjustment input bug

--- Website/src/PaletteMaker/components/Inputs/AdjustmentsInput.jsx
+++ Website/src/PaletteMaker/components/Inputs/AdjustmentsInput.jsx
@@ -1,7 +1,7 @@
 import NumberInput from "./NumberInput";
 import { useTranslation } from "react-i18next";
 
-export default function AdjustmentsInput({hue, sat, value, onHueChange, onSatChange, onValChange, disabled= false}){
+export default function AdjustmentsInput({hue, sat, val, onHueChange, onSatChange, onValChange, disabled= false}){
     //need to get adjustments
     const { t: translate } = useTranslation("palette");
 
@@ -9,7 +9,7 @@ export default function AdjustmentsInput({hue, sat, value, onHueChange, onSatCha
     <div className="flex flex-row items-center">
         <NumberInput onValueChange={onHueChange} labelText={translate("hue")} value={hue || ""} min={-360} max={360} disabled={disabled}></NumberInput>
         <NumberInput onValueChange={onSatChange} labelText={translate("sat")} value={sat || ""} min={-100} max={100} disabled={disabled}></NumberInput>
-        <NumberInput onValueChange={onValChange} labelText={translate("val")} value={value || ""} min={-100} max={100} disabled={disabled}></NumberInput>
+        <NumberInput onValueChange={onValChange} labelText={translate("val")} value={val || ""} min={-100} max={100} disabled={disabled}></NumberInput>
     </div>
     </>
 }
